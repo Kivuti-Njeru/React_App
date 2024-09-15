d4ff5febb3dedf5a6ebcec16650b382a2b643b3e
Kivuti Njeru <kivutialex4@gmail.com>
2024-09-15 09:29:36 +0300

    � click events practice

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Content = () => {
+  const [name, setName] = useState('World')
+  const changeName = () => {
+    const names = ['Alexander', 'Kefini', 'Jacob']
+    const num = Math.floor(Math.random() * names.length)
+    setName(names[num])
+  }
   return (
     <>
       <main>
-        <p>Hello World!</p>
+        <p>Hello {name}</p>
+        <button onClick={changeName}>click Me</button>
       </main>
     </>
   )
